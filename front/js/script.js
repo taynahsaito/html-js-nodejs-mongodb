edce69c0fa3f2787d972a9924263c7fc5fe583de
Taynah Saito <taynah.saito@gmail.com>
2023-11-16 12:33:36 -0300

    aula_10_nodejs_mongodb2

--- front/js/script.js
+++ front/js/script.js
@@ -7,5 +7,15 @@ async function obterFilmes(){
     const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`
 
     const filmes = (await axios.get(URLcompleta)).data
-    console.log(filmes)
+    
+    //encontrar a tabela na arvore DOM
+    let tabela = document.querySelector('.filmes') //buscando alguém que tem a classe filmes (que no caso seria o table)
+    let corpoDaTabela = tabela.getElementsByTagName('tbody')[0]
+    for (let filme of filmes){
+        let linha = corpoDaTabela.insertRow(0) //insertRow(0) para adicionar sempre na primeira linha, se quiser adicionar na última, chame insertRow sem argumentos
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
 }
